dacd5bb8b94243cb15839eef566b4494a6d3a7a0
Tristan Caulfield <tristancaulfield@gmail.com>
2021-05-26 12:15:57 +0100

    Remove logging for now

--- src/vis.jl
+++ src/vis.jl
@@ -20,7 +20,7 @@ function jslog_init(sim, stream)
         end
 
 
-        write(sim.log_stream, " {\n")
+        write(sim.log_stream, "data = {\n")
         write(sim.log_stream, "\"locations\": ")
         JSON.print(sim.log_stream, locs)
         write(sim.log_stream, ",\n\"links\": ")
@@ -33,7 +33,7 @@ end
 function jslog_end(sim)
     if jsloglevel > LOG_OFF
         time = now(sim)
-        write(sim.log_stream, "{ \"type\":\"end-log\", \"time\": $time} ]\n}\n")
+        write(sim.log_stream, "{type:\"end-log\", \"time\": $time} ]\n}\n")
     end
 end
 
@@ -44,27 +44,22 @@ end
 
 macro jslog(level, sim, obj)
 
-    return nothing
-
-#   if eval(level) > jsloglevel
-#       return nothing
-#   else
-#       return quote
-#           jslog($(esc(sim)), $(esc(obj)))
-#       end
-#   end
+  if eval(level) > jsloglevel
+      return nothing
+  else
+      return quote
+          jslog($(esc(sim)), $(esc(obj)))
+      end
+  end
 end
 
 
 macro iflog(level, code)
-
-    return nothing
-
-    # if eval(level) > jsloglevel 
-    #     return nothing
-    # else
-    #     return quote
-    #         $(esc(code))
-    #     end
-    # end
+    if eval(level) > jsloglevel
+        return nothing
+    else
+        return quote
+            $(esc(code))
+        end
+    end
 end
